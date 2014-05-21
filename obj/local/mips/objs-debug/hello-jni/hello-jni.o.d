a1d25a588b1574336efa4811c04bce881261807e
deepak kumar <deepak.s.kumar.85@gmail.com>
2014-05-21 10:51:54 -0400

    v1
    ndk hello world

---
+++ obj/local/mips/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/mips/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/string.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/sys/cdefs.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/sys/cdefs_elf.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/android/api-level.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/malloc.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/jni.h
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/string.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/sys/cdefs.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/sys/cdefs_elf.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/android/api-level.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/malloc.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-mips/usr/include/jni.h:
---
+++ obj/local/x86/objs-debug/hello-jni/hello-jni.o.d
@@ -0,0 +1,19 @@
+obj/local/x86/objs-debug/hello-jni/hello-jni.o: jni/hello-jni.c \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/string.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/sys/cdefs.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/sys/cdefs_elf.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/android/api-level.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/malloc.h \
+ C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/jni.h
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/string.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/sys/cdefs.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/sys/cdefs_elf.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/android/api-level.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/malloc.h:
+
+C:/Users/kumar.331/Documents/android-ndk-r9d/platforms/android-17/arch-x86/usr/include/jni.h:
